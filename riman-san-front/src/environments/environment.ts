09f6a9de424d86e13cf701562f8e1a847603a9fb
hamza halawa <69251476+hamza-halawa2020@users.noreply.github.com>
2024-01-02 12:54:48 +0200

    work
    work

--- riman-san-front/src/environments/environment.ts
+++ riman-san-front/src/environments/environment.ts
@@ -1,8 +1,8 @@
-// export const environment = {
-//   backEndUrl: 'https://backend.rimansan.net/public/api',
-//   imgUrl: 'https://backend.rimansan.net/public/img/',
-// };
 export const environment = {
-  backEndUrl: 'http://127.0.0.1:8000/api',
+  backEndUrl: 'https://backend.rimansan.net/public/api',
   imgUrl: 'https://backend.rimansan.net/public/img/',
 };
+// export const environment = {
+//   backEndUrl: 'http://127.0.0.1:8000/api',
+//   imgUrl: 'https://backend.rimansan.net/public/img/',
+// };
